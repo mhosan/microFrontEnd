223c0f6628b34dc04e31e9d95a3b98a58d072b75
Marcelo Hosan <mhosan@gmail.com>
2025-09-20 17:05:23 -0300

    probando mf repetidos

--- host/src/app/app.ts
+++ host/src/app/app.ts
@@ -14,9 +14,15 @@ export class App implements OnInit, OnDestroy{
 
   @ViewChild('mfe1', {read: ViewContainerRef, static: true}) listContainer!: ViewContainerRef;
   @ViewChild('mfe2', {read: ViewContainerRef, static: true}) listContainer2!: ViewContainerRef;
+  @ViewChild('mfe3', {read: ViewContainerRef, static: true}) listContainer3!: ViewContainerRef;
+  @ViewChild('mfe4', {read: ViewContainerRef, static: true}) listContainer4!: ViewContainerRef;
+  
 
   private listComponentRefmfe1: ComponentRef<any> | null = null;
   private listComponentRefmfe2  : ComponentRef<any> | null = null;
+  private listComponentRefmfe3  : ComponentRef<any> | null = null;
+  private listComponentRefmfe4  : ComponentRef<any> | null = null;
+
 
   async ngOnInit() {
     try {
@@ -29,7 +35,17 @@ export class App implements OnInit, OnDestroy{
       this.listContainer2.clear();
       this.listComponentRefmfe2=this.listContainer2.createComponent(mfe2.App);
       this.listComponentRefmfe2.changeDetectorRef.detectChanges();
-      
+
+      const mfe3 =await this.microFrontend.loadRemoteComponent(4201, 'mfe1'); 
+      this.listContainer3.clear();
+      this.listComponentRefmfe3=this.listContainer3.createComponent(mfe1.App);
+      this.listComponentRefmfe3.changeDetectorRef.detectChanges();
+
+      const mfe4 =await this.microFrontend.loadRemoteComponent(4202, 'mfe2'); 
+      this.listContainer4.clear();
+      this.listComponentRefmfe4=this.listContainer4.createComponent(mfe2.App);
+      this.listComponentRefmfe4.changeDetectorRef.detectChanges();
+
     } catch (error) {
       console.error(`Error loading mfe1 remote component:`, error);
     }
@@ -41,6 +57,12 @@ export class App implements OnInit, OnDestroy{
     }
     if (this.listComponentRefmfe2) {
       this.listComponentRefmfe2.destroy();
+    }
+    if (this.listComponentRefmfe3) {
+      this.listComponentRefmfe3.destroy();
+    }
+    if (this.listComponentRefmfe4) {
+      this.listComponentRefmfe4.destroy();
     }
   }
 
